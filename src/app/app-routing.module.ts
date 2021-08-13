e17ea0e13b7cbfe6e6ab8dc392d9d49d12746088
Robert Radnai <robert.radnai@gmail.com>
2021-08-13 14:44:09 +0200

    Moved authentication into AuthService, removed obsolete code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 import { AboutComponent } from './about/about.component';
 import { FeedbackComponent } from './feedback/feedback.component';
-import { LoginComponent } from './login/login.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { WordListChoiceComponent } from './word-list-choice/word-list-choice.component';
 
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
   { path: 'word-lists', component: WordListChoiceComponent},
   { path: '', component: WordListChoiceComponent},
   { path: 'quiz', component: QuizComponent},
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { version } from "../version";
-import { OAuthService, AuthConfig } from 'angular-oauth2-oidc';
-import { environment } from '../environments/environment';
+import { AuthService, User } from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
-  constructor(private oauthService: OAuthService) {
+
+  constructor(private authService: AuthService) {
+
+
     console.log("Vocabulary front end version: " + version.number);
-        
-    this.oauthService.configure(authCodeFlowConfig);
-    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+    authService.init();
+    
+  }
+
+  user: User;
+
+  ngOnInit() {
+    this.user = this.authService.user;
   }
 
   title = 'Vocabulary quiz demo';
@@ -25,54 +32,6 @@ export class AppComponent {
     this.showDemo = false;
   }
 
-  public login() {
-    this.oauthService.initLoginFlow();
-  }
-
-  public logout() {
-      this.oauthService.logOut();
-  }
-
-  public get userName() {
-
-      var claims = this.oauthService.getIdentityClaims();
-
-      console.log(`Claims: ${JSON.stringify(claims)}`)
-
-      if (!claims) return null;
-
-      return claims["cognito:username"];
-  }
-
 }
 
 
-export const authCodeFlowConfig: AuthConfig = {
-  // Url of the Identity Provider
-  issuer: environment.oauthIssuer,
-
-  // URL of the SPA to redirect the user to after login
-  redirectUri: environment.oauthRedirectUri,
-
-  // The SPA's id. The SPA is registerd with this id at the auth-server
-  // clientId: 'server.code',
-  clientId: environment.oauthClientId,
-
-  // Just needed if your auth server demands a secret. In general, this
-  // is a sign that the auth server is not configured with SPAs in mind
-  // and it might not enforce further best practices vital for security
-  // such applications.
-  //dummyClientSecret: 'secret',
-
-  responseType: 'token',
-
-  // set the scope for the permissions the client should request
-  // The first four are defined by OIDC.
-  // Important: Request offline_access to get a refresh token
-  // The api scope is a usecase specific one
-
-  showDebugInformation: environment.oauthShowDebugInformation,
-
-  strictDiscoveryDocumentValidation: false,
-
-};
