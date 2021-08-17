8b5c3415c53f4b3104b3c540413b2c5afe787f41
Robert Radnai <robert.radnai@gmail.com>
2021-08-17 19:36:37 +0200

    Updated redirect URI

--- src/environments/environment.develop.ts
+++ src/environments/environment.develop.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
 
   oauthIssuer: "https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_qeZgk8OhE",
-  oauthRedirectUri: "3.127.123.249:81/vocabulary",
+  oauthRedirectUri: "http://3.127.123.249:81/vocabulary",
   oauthClientId: '546p2qilk2rcd2l9f5ajgjlikl',
   oauthShowDebugInformation: false
 };
