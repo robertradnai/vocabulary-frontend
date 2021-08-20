3b90a26337baa2220ad8d383855a794e4b3c875d
Robert Radnai <robert.radnai@gmail.com>
2021-08-20 17:44:28 +0200

    Removed interceptor for guest user management

--- src/app/http-interceptors/index.ts
+++ src/app/http-interceptors/index.ts
@@ -2,10 +2,8 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { DefaultOAuthInterceptor } from 'angular-oauth2-oidc';
-import { NoopInterceptor } from './token-interceptor';
 
 /** Http interceptor providers in outside-in order */
 export const httpInterceptorProviders = [
-  { provide: HTTP_INTERCEPTORS, useClass: NoopInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: DefaultOAuthInterceptor, multi: true },
 ];
